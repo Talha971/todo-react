f0ffbdd91850f3953482731f51e1b5ed1da5289b
Talha971 <talhatahir971@gmail.com>
2024-12-18 13:13:56 +0500

    functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
+
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState([])
+  const [currentItem, setCurrentItem] = useState('')
+  const addTodoItems = (e) => {
+    e.preventDefault();
+    const currentTodos = [...todos]
+    setTodos([
+      ...currentTodos,
+      {
+        id: currentTodos.length,
+        name: currentItem,
+      },
+    ])
+    console.log(todos);
+    setCurrentItem('')
+  }
+  const del_fun = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
   return (
-    <>
-      <h2 style={{color:'aqua'}}>Todo Generator</h2>
+    <div className='main'>
+      <h2 style={{ color: 'aqua', marginBottom: "10px" }}>Todo Generator</h2>
       <div className='todo-container'>
         <ul>
-          <li>
-            <p>Item 1</p>
-            <button className='del-btn'>Delete</button>
-          </li>
-          <li>
-            <p>Item 2</p>
-            <button className='del-btn'>Delete</button>
-          </li>
-
-          <li>
-            <p>Item 3</p>
-            <button className='del-btn'>Delete</button>
-          </li>
-
+          {
+            todos.map((todo, index) => {
+              return <li key={index}>
+                <p>{todo.name}</p>
+                <button onClick={() => del_fun(todo.id)} className='del-btn'>Delete</button>
+              </li>
+            })
+          }
         </ul>
       </div>
-      <div>
-        <input type="text" placeholder='Enter Todo'/>
-        <input type="submit" />
-      </div>
+      <form onSubmit={addTodoItems} action="">
+        <input type="text" value={currentItem} onChange={(e) => setCurrentItem(e.target.value)} placeholder='Enter Todo' />
+        <input type="submit" value='Add Todo' />
+      </form>
+    </div>
 
-
-    </>
   )
 }
 
